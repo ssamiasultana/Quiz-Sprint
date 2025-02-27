b425cb0732a24e7786be0a85af5a143d3ea4978d
Rahul-Roy <rahul.roy16799@gmail.com>
2025-02-27 17:57:59 +0600

    Updated Quiz File

--- src/quiz/application/Quiz.java
+++ src/quiz/application/Quiz.java
@@ -49,17 +49,17 @@ public class Quiz extends JFrame implements ActionListener {
         question.setFont(new Font("Tahoma", Font.PLAIN, 24));
         add(question);
         
-        questions[0][0] = "Which is used to find and fix bugs in the Java programs.?";
-        questions[0][1] = "JVM";
-        questions[0][2] = "JDB";
-        questions[0][3] = "JDK";
+        questions[0][0] = "Which component converts java bytecode to machine code at runtime?";
+        questions[0][1] = "JDK";
+        questions[0][2] = "JIT";
+        questions[0][3] = "JVM";
         questions[0][4] = "JRE";
 
-        questions[1][0] = "What is the return type of the hashCode() method in the Object class?";
-        questions[1][1] = "int";
-        questions[1][2] = "Object";
-        questions[1][3] = "long";
-        questions[1][4] = "void";
+        questions[1][0] = "Which keyword is used to prevent a method from being overridden in Java?";
+        questions[1][1] = "static";
+        questions[1][2] = "final";
+        questions[1][3] = "private";
+        questions[1][4] = "protected";
 
         questions[2][0] = "Which package contains the Random class?";
         questions[2][1] = "java.util package";
@@ -67,11 +67,11 @@ public class Quiz extends JFrame implements ActionListener {
         questions[2][3] = "java.awt package";
         questions[2][4] = "java.io package";
 
-        questions[3][0] = "An interface with no fields or methods is known as?";
-        questions[3][1] = "Runnable Interface";
-        questions[3][2] = "Abstract Interface";
-        questions[3][3] = "Marker Interface";
-        questions[3][4] = "CharSequence Interface";
+        questions[3][0] = "Which Java feature enables methods with the same name but different parameters?";
+        questions[3][1] = "Method Overriding";
+        questions[3][2] = "Method Overloading";
+        questions[3][3] = "Polymorphism";
+        questions[3][4] = "Abstrction";
 
         questions[4][0] = "In which memory a String is stored, when we create a string using new operator?";
         questions[4][1] = "Stack";
@@ -91,34 +91,34 @@ public class Quiz extends JFrame implements ActionListener {
         questions[6][3] = "extends";
         questions[6][4] = "export";
 
-        questions[7][0] = "In java, jar stands for?";
-        questions[7][1] = "Java Archive Runner";
-        questions[7][2] = "Java Archive";
-        questions[7][3] = "Java Application Resource";
-        questions[7][4] = "Java Application Runner";
+        questions[7][0] = "Which of the following is a common type of code smell?";
+        questions[7][1] = "Type Casting";
+        questions[7][2] = "Long Method";
+        questions[7][3] = "Exception Propagation";
+        questions[7][4] = "Short Circuit";
 
-        questions[8][0] = "Which of the following is a mutable class in java?";
-        questions[8][1] = "java.lang.StringBuilder";
-        questions[8][2] = "java.lang.Short";
-        questions[8][3] = "java.lang.Byte";
-        questions[8][4] = "java.lang.String";
+        questions[8][0] = "Which code smell occurs when a method relies too much on another class’s data?";
+        questions[8][1] = "Swithch Statements";
+        questions[8][2] = "Long Method";
+        questions[8][3] = "Data Clump";
+        questions[8][4] = "Feature Envy";
 
-        questions[9][0] = "Which of the following option leads to the portability and security of Java?";
-        questions[9][1] = "Bytecode is executed by JVM";
-        questions[9][2] = "The applet makes the Java code secure and portable";
-        questions[9][3] = "Use of exception handling";
-        questions[9][4] = "Dynamic binding between objects";
+        questions[9][0] = "Which block always executes in a try-catch-finally structure?";
+        questions[9][1] = "finally";
+        questions[9][2] = "catch";
+        questions[9][3] = "try";
+        questions[9][4] = "throw";
         
-        answers[0][1] = "JDB";
-        answers[1][1] = "int";
+        answers[0][1] = "JVM";
+        answers[1][1] = "final";
         answers[2][1] = "java.util package";
-        answers[3][1] = "Marker Interface";
+        answers[3][1] = "Method Overloading";
         answers[4][1] = "Heap memory";
         answers[5][1] = "Remote interface";
         answers[6][1] = "import";
-        answers[7][1] = "Java Archive";
-        answers[8][1] = "java.lang.StringBuilder";
-        answers[9][1] = "Bytecode is executed by JVM";
+        answers[7][1] = "Long Method";
+        answers[8][1] = "Feature Envy";
+        answers[9][1] = "finally";
         
         opt1 = new JRadioButton();
         opt1.setBounds(170, 520, 700, 30);
